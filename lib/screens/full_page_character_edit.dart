24307727591fb960aab362da878f5e8c9dc1c440
Michael VP <michaelvp@mvpitsolutions.com>
2022-03-19 03:14:36 -0400

    character display mostly setup

--- lib/screens/full_page_character_edit.dart
+++ lib/screens/full_page_character_edit.dart
@@ -2,7 +2,7 @@ import 'package:dnddmcompanion/models/character.dart';
 import 'package:dnddmcompanion/widgets/character_detail.dart';
 import 'package:flutter/material.dart';
 
-class FullPageCharacterEdit extends StatelessWidget {
+class FullPageCharacterEdit extends StatefulWidget {
   final Character character;
 
   const FullPageCharacterEdit({required this.character, Key? key})
@@ -11,9 +11,24 @@ class FullPageCharacterEdit extends StatelessWidget {
   static const routeName = '/character';
 
   @override
+  State<FullPageCharacterEdit> createState() => _FullPageCharacterEditState();
+}
+
+class _FullPageCharacterEditState extends State<FullPageCharacterEdit> {
+  late Character character;
+
+  @override
+  void initState() {
+    super.initState();
+    character = widget.character.decouple(widget.character);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.save))],
+      ),
       body: CharacterDetail(character: character),
     );
   }
