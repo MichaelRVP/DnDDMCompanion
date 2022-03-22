fea9a7e4e9906de242bb0dec05bc31980135ae7c
Michael VP <michaelvp@mvpitsolutions.com>
2022-03-22 11:22:02 -0400

    defult spacing on overlay

--- lib/widgets/maps/grid_map_overlay.dart
+++ lib/widgets/maps/grid_map_overlay.dart
@@ -1,5 +1,6 @@
 import 'package:dnddmcompanion/widgets/maps/draggable_map.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class GridMapOverlay extends StatefulWidget {
   final String imageString;
@@ -16,13 +17,18 @@ class GridMapOverlay extends StatefulWidget {
 class _GridMapOverlayState extends State<GridMapOverlay> {
   late int totalWidthCells;
   late int totalHeightCells;
+  late double topCellPadding;
+  late double leftCellPadding;
+  late double cellSizing;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    totalHeightCells = 40;
-    totalWidthCells = 73;
+    totalHeightCells = 42;
+    totalWidthCells = 66;
+    topCellPadding = 50;
+    leftCellPadding = 16;
+    cellSizing = 10;
   }
 
   @override
@@ -31,13 +37,13 @@ class _GridMapOverlayState extends State<GridMapOverlay> {
       builder: ((context, constraints) {
         return SizedBox(
           width: constraints.maxWidth * .8,
-          child: InteractiveViewer(
-            boundaryMargin: const EdgeInsets.all(80),
-            minScale: 0.5,
-            maxScale: 4,
-            child: Column(
-              children: [
-                Stack(
+          child: Column(
+            children: [
+              InteractiveViewer(
+                boundaryMargin: const EdgeInsets.all(80),
+                minScale: 0.5,
+                maxScale: 4,
+                child: Stack(
                   children: [
                     Container(
                       decoration: BoxDecoration(
@@ -61,93 +67,143 @@ class _GridMapOverlayState extends State<GridMapOverlay> {
                     SizedBox(
                       width: constraints.maxWidth * .8,
                       height: 732,
-                      child: GridView.count(
-                        crossAxisCount: totalWidthCells,
-                        children: List.generate(
-                            totalWidthCells * totalHeightCells, (index) {
-                          return GestureDetector(
-                            onTap: () {
-                              print(index);
-                            },
-                            child: Container(
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                    color: Colors.black.withOpacity(.3)),
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(leftCellPadding,
+                            topCellPadding, leftCellPadding, topCellPadding),
+                        child: GridView.count(
+                          crossAxisCount: totalWidthCells,
+                          childAspectRatio: 1,
+                          children: List.generate(
+                              totalWidthCells * totalHeightCells, (index) {
+                            return GestureDetector(
+                              onTap: () {
+                                //TODO get the x y from this
+                                print(index);
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Color((math.Random().nextDouble() *
+                                                0xFFFFFF)
+                                            .toInt())
+                                        .withOpacity(.4)),
                               ),
-                            ),
-                          );
-                        }),
+                            );
+                          }),
+                        ),
                       ),
                     ),
                   ],
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.blue.withOpacity(.4)),
-                  child: Row(
-                    children: [
-                      const Spacer(),
-                      IconButton(
-                        onPressed: () {
-                          setState(() {
-                            totalHeightCells--;
-                          });
-                        },
-                        icon: const Icon(Icons.remove),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.blue.withOpacity(.4)),
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          topCellPadding > 0 ? topCellPadding-- : null;
+                        });
+                      },
+                      icon: const Icon(Icons.remove),
+                    ),
+                    Text('$topCellPadding'),
+                    const Text(' Top Padding'),
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          topCellPadding++;
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.add,
                       ),
-                      const Icon(
+                    ),
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          leftCellPadding > 0 ? leftCellPadding-- : null;
+                        });
+                      },
+                      icon: const Icon(Icons.remove),
+                    ),
+                    Text('$leftCellPadding'),
+                    const Text(' Left Padding'),
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          leftCellPadding++;
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.add,
+                      ),
+                    ),
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          totalHeightCells--;
+                        });
+                      },
+                      icon: const Icon(Icons.remove),
+                    ),
+                    Text('$totalHeightCells'),
+                    const Icon(
+                      Icons.unfold_more,
+                      size: 40,
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          totalHeightCells++;
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.add,
+                      ),
+                    ),
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          totalWidthCells--;
+                        });
+                      },
+                      icon: const Icon(Icons.remove),
+                    ),
+                    Text('$totalWidthCells'),
+                    Transform.rotate(
+                      angle: radians(90),
+                      child: const Icon(
                         Icons.unfold_more,
                         size: 40,
                       ),
-                      IconButton(
-                        onPressed: () {
-                          setState(() {
-                            totalHeightCells++;
-                          });
-                        },
-                        icon: const Icon(
-                          Icons.add,
-                        ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          totalWidthCells++;
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.add,
                       ),
-                      const Spacer(),
-                      IconButton(
-                        onPressed: () {
-                          setState(() {
-                            totalWidthCells--;
-                          });
-                        },
-                        icon: const Icon(Icons.remove),
+                    ),
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.save,
                       ),
-                      Transform.rotate(
-                        angle: radians(90),
-                        child: const Icon(
-                          Icons.unfold_more,
-                          size: 40,
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          setState(() {
-                            totalWidthCells++;
-                          });
-                        },
-                        icon: const Icon(
-                          Icons.add,
-                        ),
-                      ),
-                      const Spacer(),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.save,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         );
       }),
